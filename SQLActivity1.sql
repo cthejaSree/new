0199c60f818c957749271a6e24c1c9481f8562f4
cthejaSree <thejac25@in.ibm.com>
2024-09-05 13:56:45 +0530

    Create SQLActivity1.sql

---
+++ SQLActivity1.sql
@@ -0,0 +1,49 @@
+
+create table salesman( 
+    salesman_id int, 
+    salesman_name Varchar2(32), 
+    salesman_city Varchar2(32) 
+    commission int, 
+);
+
+Describe salesman
+
+
+create table salesman( 
+    salesman_id int, 
+    salesman_name Varchar2(32), 
+    salesman_city Varchar2(32) 
+    commission int 
+);
+
+Describe salesman
+
+
+create table salesman( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32) 
+    commission int 
+);
+
+Describe salesman
+
+
+create table salesman( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32) 
+    commission int 
+);
+
+  Describe salesman
+
+
+create table salesman( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+  Describe salesman
